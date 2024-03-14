0f92d9d63bf3c13e2fa6d6b2621dc77d52123109
Vinay B <vinay.bayyapunedi@gmail.com>
2024-03-14 12:45:29 +1100

    Data Annotation Completed

--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,17 @@
+import os
 import subprocess
+# Path to the directory containing the LabelImg executable script
+directory_to_add = "/Users/vinay/Library/Python/3.7/bin"
 
-subprocess.run(["labelImg"])
+# Get the current PATH environment variable
+current_path = os.environ.get('PATH', '')
+
+# Append the directory to the PATH if it's not already there
+if directory_to_add not in current_path:
+    os.environ['PATH'] = f"{directory_to_add}:{current_path}"
+    print("Directory added to PATH successfully.")
+else:
+    print("Directory already exists in PATH.")
+    
+    
+subprocess.run("labelImg")
